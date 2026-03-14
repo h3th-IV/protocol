5292da52aa5a8a7fe277abb6185411cdabafa506
h3th-iv <samuelbonux10@gmail.com>
2026-03-14 18:12:52 +0100

    threader

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -13,15 +14,40 @@ func main() {
 		log.Fatal("Unable to open file")
 	}
 
-	for {
-		data := make([]byte, 8)
-		n, err := file.Read(data)
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println("Done reading file content")
-				os.Exit(0)
-			}
-		}
-		fmt.Printf("Read: %d\n", len(data[:n]))
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("Read: %v\n", line)
 	}
+
+}
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				if err == io.EOF {
+					fmt.Println("Done reading file content")
+					os.Exit(0)
+				}
+			}
+
+			data = data[:n]
+			if nlIndex := bytes.IndexByte(data, '\n'); nlIndex != -1 {
+				str += string(data[:nlIndex])
+				out <- str
+				data = data[nlIndex+1:]
+				str = ""
+			}
+
+			str += string(data)
+		}
+	}()
+	return out
 }
